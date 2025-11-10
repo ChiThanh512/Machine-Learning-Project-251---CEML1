3bd300918d2fdb779f093f09a608eea666e7ec97
Le Duc Tai <ldtai061205@gmail.com>
2025-11-10 23:50:02 +0700

    HMM_thay đổi thông số trích xuất (n_fft)

--- HPMR/modules/preprocessing.py
+++ HPMR/modules/preprocessing.py
@@ -16,9 +16,9 @@ from sklearn.model_selection import train_test_split
 
 ## Định nghĩa các tham số cơ bản
 SR = 22050 #tần số lấy mẫu
-N_FFT = 512 #int(0.025*SR) # khoảng lấy mẫu fft 25ms
-N_HOP = 256 #int(0.010*SR) # bước nhảy giữa 2 frame
-N_MFCC = 40
+N_FFT = 256 #int(0.025*SR) # khoảng lấy mẫu fft 25ms
+N_HOP = 128 #int(0.010*SR) # bước nhảy giữa 2 frame
+N_MFCC = 13
 N_MELS =40
 pre_emphasis = 0.95
 def extract_features(file_path):
